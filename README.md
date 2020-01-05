c4739652900577efceba05d087250cdbf333b905
YaboSun <yabosun@163.com>
2020-01-05 11:22:51 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# spring-boot-web-restfulcrud
+
+https://www.bilibili.com/video/av20965295/?p=29
--- src/main/java/com/ybsun/web/WebApplication.java
+++ src/main/java/com/ybsun/web/WebApplication.java
@@ -3,6 +3,10 @@ package com.ybsun.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Sun
+ * @date 2020/1/5 11:17
+ */
 @SpringBootApplication
 public class WebApplication {
 
